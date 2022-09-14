07192b77f4c295d74086b150160be5f3e9dbd55e
Aleksey <a_zhuravlyov@bk.ru>
2022-09-14 12:12:40 +0300

    Finished Methods Enter and OutPut

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿static void Enter()
+{
+    Console.WriteLine("Введите количество жлементов массива");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    string[] array = new string[N];
+
+    for(int i = 0; i < array.Length;i++)
+    {
+        Console.Write("Введите элемент массива - ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }  
+    OutPut(array); 
+}
+static void OutPut(string[] lit)
+{
+    Console.Write("[");
+    for(int i = 0; i < lit.Length; i++)
+    {
+        Console.Write(lit[i]);
+        if( i == lit.Length - 1) Console.Write("");
+        else Console.Write(", ");
+    }
+    Console.Write("]");
+}
+Enter();
